448f3147b4e080819974545ebde35d7771b46b53
zearkiatos <caprilespe@outlook.com>
2024-09-13 15:40:36 -0300

    Adapt the arduino code to my protoboard configuration

---
+++ NodemcuMeasurer/configexample.h
@@ -0,0 +1,12 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+#define MQTT_PASSWORD ""
+#define MQTT_HOSTNAME ""
+#define MQTT_USERNAME ""
+#define LOCATION ""
+
+#endif
